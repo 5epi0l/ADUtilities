e8d9039c740efcd25b66b43bde0389f53d9db78a
5epi0l <91630053+5epi0l@users.noreply.github.com>
2025-11-16 06:15:06 +0530

    Add files via upload

---
+++ clock_skew.py
@@ -0,0 +1,38 @@
+import requests
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--url", help="Target URL", required=True)
+
+args = parser.parse_args()
+
+
+
+
+def get_date():
+    print("[*] Fetching time")
+    try:
+        r = requests.get(args.url + "/:5985/WSMAN")
+        date_header = r.headers.get("Date")
+        time = date_header.split()[4]
+        if time:
+            print("[*] time fetched: ", time)
+            return time
+        else:
+            print("[-] Could not get remote time")
+    except Exception as e:
+        print("[*] An error has occured: ", e)
+
+def set_date():
+    time = get_date()
+    try:
+        print("[*] Syncing time")
+        os.system("sudo timedatectl set-ntp off")
+        os.system(f"sudo date -u --set {time}")
+        print("[*] time Synced")
+    except Exception as e:
+        print("[-] An error has occured: ", e)
+
+
+set_date()
